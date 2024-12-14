9383e08b9f52a42bfaef8552fca4128e3d5d121d
Ehtesham Irshad <sardarsham79@gmail.com>
2024-12-14 23:22:45 +0500

    My portfolio

---
+++ src/content_option.js
@@ -0,0 +1,156 @@
+const logotext = "Ehtesham";
+const meta = {
+    title: "Ehtesham Irshad",
+    description: "I’m Ehtesham Irshad front end devloper,currently working in Islamabad Pakistan",
+};
+
+const introdata = {
+    title: "I’m Ehtesham Irshad",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I develop mobile apps",
+    },
+    description: "I’m Ehtesham Irshad, a Front-End Developer who loves coding and building cool websites and mobile apps. I focus on creating smooth, user-friendly experiences using the latest technologies.",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "About my self",
+    aboutme: "I’m Ehtesham Irshad, a Front-End Developer with experience in React.js and Mendix. With over 3.5 years of work, I build responsive websites and apps that are easy to use. I focus on creating simple and efficient designs, improving development processes, and working with teams to turn ideas into reality.",
+};
+const worktimeline = [{
+        jobtitle: "Front End Developer",
+        where: "Inov8",
+        date: "April 2024",
+    },
+    {
+        jobtitle: "Front End Developer",
+        where: "LowCodeX",
+        date: "June 2023",
+    },
+    {
+        jobtitle: "Degital Marketer",
+        where: "Upwork",
+        date: "February 2022",
+    },
+];
+
+const skills = [{
+        name: "React",
+        value: 90,
+    },
+    {
+        name: "React Native",
+        value: 70,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "I create user-friendly interfaces that prioritize functionality and simplicity. My designs focus on improving usability and ensuring seamless interaction. From wireframes to final designs, I deliver solutions tailored to your needs and audience.",
+    },
+    {
+        title: "Mobile Apps",
+        description: "As a front-end developer, I create responsive, user-friendly mobile interfaces for iOS and Android. I focus on optimizing performance and delivering seamless, engaging experiences using modern frameworks and clean design principles.",
+    },
+    {
+        title: "Wordpress Design",
+        description: "I design custom, responsive WordPress websites that are visually appealing and user-friendly. I create functional themes tailored to your brand, ensuring seamless navigation and optimized performance across devices.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "sardarsham79@gmail.com",
+    YOUR_FONE: "+923180875014",
+    description: "Feel free to reach out for inquiries, collaborations, or any questions you may have!",
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/Ehteshamirshad123",
+    facebook: "https://www.facebook.com/sardar.shaam.942/",
+    linkedin: "https://www.linkedin.com/in/ehtesham-irshad-b70996194/",
+    twitter: "https://twitter.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
